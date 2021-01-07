07863fce96d9107a0b0105a87e62a00d12e08077
boundless-lu <dailu_1993@sina.cn>
2021-01-07 17:38:34 +0800

    OCT commit annotation

--- conversion/src/main/java/com/study/thinking/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
+++ conversion/src/main/java/com/study/thinking/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
@@ -15,6 +15,17 @@ public class SpringCustomizedPropertyEditorDemo {
 
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("META-INF/property-editors-context.xml");
 
+        // AbstractApplicationContext --> "conversionService" ConversionService Bean -->
+        //ConfigurableBeanFactory#setConversionService(ConversionService) -->
+        //AbstractAutowireCapableBeanFactory#instatiteBean -->
+        //AbstractBeanFactory#getConversionService -->
+        //BeanDefinition --> BeanWrapper --> 属性转换(数据来源 PropertyValues)
+        //setPropertyValues(PropertyValues) --> TypeConverter#convertIfNecessary
+        //TypeConverterDelegate#convertIfNecessary  --> PropertyEditor  or  ConversionService
+
+
+
+
         final User user = context.getBean("user", User.class);
 
         System.out.println(user);
